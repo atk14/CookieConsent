680a0d32940c3027436902a4ef3efc8271c7cc3e
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-12-06 14:49:28 +0100

    Helper cookie_consent_datalayer_command tuned

--- src/app/helpers/function.cookie_consent_datalayer_command.php
+++ src/app/helpers/function.cookie_consent_datalayer_command.php
@@ -1,17 +1,21 @@
 <?php
 
 function smarty_function_cookie_consent_datalayer_command($params, $template) {
-	$granted = [];
 	$gtm_consent_equivalents = [
 		"advertising" => "ad_storage",
 		"analytics" => "analytics_storage",
 		"necessary" => "functionality_storage",
-#		"personalization_storage",
-#		"security_storage",
+#		"??" => "personalization_storage",
+#		"??" => "security_storage",
 	];
-	foreach( CookieConsentCategory::FindAll() as $cat ) {
-		if ( CookieConsent::Accepted($cat) ) {
-			$granted[$gtm_consent_equivalents[$cat->getCode()]] = "granted";
+	$settings = CookieConsent::GetSettings();
+
+	$granted = [];
+	foreach( CookieConsentCategory::GetAllInstances() as $cat ) {
+		$code = $cat->getCode();
+		if ( !isset($gtm_consent_equivalents[$code]) ){ continue; }
+		if ( $settings->accepted($cat) ) {
+			$granted[$gtm_consent_equivalents[$code]] = "granted";
 		}
 	}
 
