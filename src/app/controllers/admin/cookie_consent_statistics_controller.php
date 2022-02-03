b66ab25ff63040cc63f307efdfbe17a1336e728b
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-02-03 11:00:07 +0100

    local_scripts/export_cookie_consent_statistics fixed

--- src/app/controllers/admin/cookie_consent_statistics_controller.php
+++ src/app/controllers/admin/cookie_consent_statistics_controller.php
@@ -2,6 +2,7 @@
 class CookieConsentStatisticsController extends AdminController {
 
 	function index(){
+		global $ATK14_GLOBAL;
 		$this->page_title = _("Statistika klikání na souhlas s použitím cookies");
 		$this->breadcrumbs[] = _("Statistika klikání");
 
@@ -10,7 +11,8 @@ class CookieConsentStatisticsController extends AdminController {
 			$days = $this->params->getInt("days");
 		}
 		if($this->params->getString("format")==="csv"){
-			$cmd = "php ".ATK14_DOCUMENT_ROOT."/local_scripts/export_cookie_consent_statistics".($days ? " {$days}" : "");
+			$env = strtolower($ATK14_GLOBAL->getEnvironment()); // "production", "development"
+			$cmd = "ATK14_ENV=$env php ".ATK14_DOCUMENT_ROOT."/local_scripts/export_cookie_consent_statistics".($days ? " {$days}" : "");
 			$content = `$cmd`;
 			$this->render_template = false;
 			$this->response->setContentType("text/csv");
